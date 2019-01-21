afcbff69f5f170f0ac2eaf42d616be026388dca0
Reikem <jimmymeneses11@gmail.com>
2019-01-21 17:08:41 -0300

    Conexion, validacion y envio de sistema de envio de mensajes a whatsapp

---
+++ Soluccion_MesajeWhatsApp/MesajeWhatsApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MesajeWhatsApp
+{
+    partial class PantallaPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btn_enviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Location = new System.Drawing.Point(95, 30);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(235, 13);
+            this.lbl_titulo.TabIndex = 0;
+            this.lbl_titulo.Text = "Inserte un Mensaje que se enviar por WhatsApp";
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(59, 59);
+            this.txtMensaje.Multiline = true;
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(344, 119);
+            this.txtMensaje.TabIndex = 1;
+            // 
+            // btn_enviar
+            // 
+            this.btn_enviar.Location = new System.Drawing.Point(126, 197);
+            this.btn_enviar.Name = "btn_enviar";
+            this.btn_enviar.Size = new System.Drawing.Size(162, 31);
+            this.btn_enviar.TabIndex = 2;
+            this.btn_enviar.Text = "Enviar Mensaje";
+            this.btn_enviar.UseVisualStyleBackColor = true;
+            this.btn_enviar.Click += new System.EventHandler(this.btn_enviar_Click);
+            // 
+            // PantallaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 262);
+            this.Controls.Add(this.btn_enviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.lbl_titulo);
+            this.Name = "PantallaPrincipal";
+            this.Text = "Formulario EnviarMensaje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btn_enviar;
+    }
+}
+
---
+++ Soluccion_MesajeWhatsApp/MesajeWhatsApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//alumno de Ing. en Informatica Jimmy Meneses
+namespace MesajeWhatsApp
+{
+    public partial class PantallaPrincipal : Form
+    {
+        public PantallaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_enviar_Click(object sender, EventArgs e)
+        {//para enviar mensaje en nice api debes registrar el numero en la api o pagina de nice api
+
+            //id dada por niceApi, para conectarlos a ella
+            string id_api = "CK0NRnybG0G2ZiO8VBXQg2ppbW15bWVuZXNlczExX2F0X2dtYWlsX2RvdF9jb20=";
+            string numero = "+56974015393";//numero de telefono que enviaremos el mensaje por Whatsapp 
+            string mensaje = txtMensaje.Text;//txt donde se escribira el mensaje
+
+            if (mensaje==null)//validación si no hay mensaje no enviara nada
+            {
+                MessageBox.Show("No se ha enviado el Mensaje", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //mensaje de advertencia que nos enviara el mensaje.
+            }
+            else
+            {
+                try
+                {//preguntamos si desea enviar ese mensaje
+                    DialogResult resul = MessageBox.Show("¿Seguro que quiere enviar el mensaje?", "Enviar mensaje", MessageBoxButtons.YesNo);
+                    if (resul == DialogResult.Yes)
+                    {
+                        //atributos 
+                        string url = "https://niceapi.net/API";//direccion de la api
+                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);//capturamos la url de la api para conectarnos
+                        request.Method = "POST";//metodo utilizado
+                        request.ContentType = "application/x-www-form-urlencoded";
+                        request.Headers.Add("X-APIId", id_api);
+                        request.Headers.Add("X-APIMobile", numero);
+                        using (System.IO.StreamWriter stream = new System.IO.StreamWriter(request.GetRequestStream()))
+                        {
+                            stream.Write(mensaje);//capturamos el mensaje que se enviara
+                        }
+                        using (System.IO.StreamReader write = new System.IO.StreamReader(request.GetResponse().GetResponseStream()))
+                        {
+                            Console.WriteLine(write.ReadToEnd());//enviamos el mensaje
+                        }
+                        //mensaje de exito
+                        MessageBox.Show("El mensaje ha sido enviado", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        txtMensaje.Clear();
+                    }
+
+                }
+                catch (SystemException ji)
+                {
+                    //excepcion capturada
+                    Console.WriteLine(ji.Message);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        }
+    }
+
